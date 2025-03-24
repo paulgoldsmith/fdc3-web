8b03435ef2506be969a7951eb0f362daf38ccbf9
Paul Goldsmith <paul.goldsmith@aplink.net>
2025-03-24 15:28:57 -0400

    Improve legibility of ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,7 +36,17 @@ const ignores = globalIgnores(
           ]
     );
 
-
+const apacheLicenseHeader =
+`/* Morgan Stanley makes this available to you under the Apache License,
+ * Version 2.0 (the "License"). You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership. Unless required by applicable law or agreed
+ * to in writing, software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions
+ * and limitations under the License. */`;
+ 
 const rules = {
     'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
     'no-unused-vars': 'off', // typescript already warns about unused but allows some like _someVar
@@ -64,16 +74,7 @@ const rules = {
     'prettier/prettier': ['error', prettierConfig], // runs prettier
     'license-header/header': [
         'error',
-        [
-`/* Morgan Stanley makes this available to you under the Apache License,
- * Version 2.0 (the "License"). You may obtain a copy of the License at
- *      http://www.apache.org/licenses/LICENSE-2.0.
- * See the NOTICE file distributed with this work for additional information
- * regarding copyright ownership. Unless required by applicable law or agreed
- * to in writing, software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions
- * and limitations under the License. */`]
+        [ apacheLicenseHeader ]
     ], // OSS license header
 };
 
